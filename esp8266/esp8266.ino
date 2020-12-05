a75e41890b8fd31d3f4ad712399acc4bb39a5d88
Ruben Horn <5367484+rubenhorn@users.noreply.github.com>
2020-12-05 13:02:37 +0100

    Update esp8266.ino

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -3,7 +3,7 @@
 
 #include "config.h"
 
-#define PIN_LED D4 /*TODO is this correct??*/
+#define PIN_LED D4
 #define PIN_STATUS D6
 #define PIN_POWER D7
 #define INTERVAL_SEND_STATUS 1000
@@ -55,10 +55,8 @@ void setup() {
     pinMode(PIN_LED, OUTPUT);
     digitalWrite(PIN_LED, LOW);
     delay(500);
-    Serial.begin(115200);//TODO
     WiFi.begin(SSID, WIFI_PASSWORD);
     for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
-        Serial.print(".");// TODO
         delay(1000);
     }
     client.onMessage(onMessageCallback);
@@ -66,11 +64,6 @@ void setup() {
     client.setInsecure(); // Do not validate certificates
     client.connect(WS_URL);    
     digitalWrite(PIN_LED, HIGH);
-
-    // Send a message TODO and below
-    client.send("Hi Server!");
-    // Send a ping
-    client.ping();
 }
 
 void loop() {
